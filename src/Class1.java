9e840a19022be275bf8ac7add9a9ab5f82f93841
etarasova88 <eatarasova88@gmail.com>
2019-04-15 16:56:20 +0300

    Initial commit Create task4

---
+++ src/Class1.java
@@ -0,0 +1,15 @@
+public class Class1 {
+    public static void main (String[] args) {
+        int age=10;
+        int height=age+165;
+        float weight=56.495f;
+        String name="Nemo";
+
+        System.out.println ("Hi there!");
+        System.out.print ("How are you doing?\n");
+        System.out.printf ("Age %d \nHeight %d \n", age, height);
+        System.out.printf ("Name: %s \nWeight: %.1f \n", name, weight);
+    }
+
+
+}
