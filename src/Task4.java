6fac6ce88cbbfd23597a6549b4cbb03ce497b857
etarasova88 <eatarasova88@gmail.com>
2019-04-19 19:12:08 +0300

    completed Task4

--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Task4 {
+    public static void main(String [] args) {
+
+
+       Scanner in = new Scanner(System.in);
+       System.out.print("Ввести бинарное число: ");
+            String figure = in.nextLine();
+
+        int res = Integer.parseInt(figure, 2);
+
+        System.out.print("Результат (int): ");
+        System.out.println(res);
+    }
 }
+
+
