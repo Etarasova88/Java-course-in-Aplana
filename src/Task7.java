006b511350ba9a9f7d511a056415b5f5e7889fd2
etarasova88 <eatarasova88@gmail.com>
2019-04-25 17:02:32 +0300

    completed Task7

---
+++ src/Task7.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+
+public class Task7 {
+
+    public static void main(String [] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Ввести число T: ");
+        String t1 = in.nextLine();
+
+        int t=Integer.parseInt(t1);
+        int x=7;
+        int y=23;
+        int z=65;
+
+    if (t == x)
+        System.out.print("Данное значение имеется в константе  X\n");
+
+    else if (t == y)
+        System.out.print("Данное значение имеется в константе Y\n");
+
+    else if (t == z)
+        System.out.print("Данное значение имеется в константе Z\n");
+
+
+           else
+            System.out.print("Такой константы нет!\n");
+
+
+  /*  case 1: t = 7;
+        System.out.print("Данное значение имеется в константах" + x + "\n");
+    break;
+
+    case t = 23:
+        System.out.print("Данное значение имеется в константах" + y + "\n");
+    break;
+
+    case t = 65:
+        System.out.print("Данное значение имеется в константах" + z + "\n");
+        break; **/
+        }
+    }
+
+
+
