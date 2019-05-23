f49ec8efc2acefe61f6a9c64f02572971381cbd9
etarasova88 <eatarasova88@gmail.com>
2019-05-23 16:02:52 +0300

    corrected Task10

--- src/Task10.java
+++ src/Task10.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
-import java.util.Arrays;
+
 
 public class Task10 {
     public static void main(String[] args) {
 
 
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите количество строк: ");
+        System.out.print("Введите количество столбцов: ");
         int number1 = Integer.parseInt(in.nextLine());
 
-        System.out.print("Введите количество столбцов: ");
+        System.out.print("Введите количество строк: ");
         int number2 = Integer.parseInt(in.nextLine());
 
         int multiply[][] = new int[number2][number1];
 
-        System.out.println("Введите числовые значения через Enter в количестве " + number1*number2 + "шт: \n");
+        System.out.println("Введите числовые значения через Enter в количестве " + number1*number2 + "шт:");
 
         for (int i = 0; i < number2; i++) {
             for (int j = 0; j < number1; j++) {
@@ -23,11 +23,11 @@ public class Task10 {
             }
         }
 
-        System.out.println("Элементы массива с результатом умножения на 3: \n");
-        for (int i = 0; i < 1; i++) {
-            for (int j = 0; j < number1; j++) {
-                System.out.print(multiply[i][j] + " * 3 = " + multiply[i][j]*3 + ", ");
-            }
+        System.out.println("Элементы массива с результатом умножения на 3:");
+       // for (int i = 0; i < number2; i++) {
+         for (int j = 0; j < number1; j++) {
+                System.out.print(multiply[0][j] + " * 3 = " + multiply[0][j]*3 + ", ");
+           //}
         }
     }
 }
