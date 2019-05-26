47a2580ffebc4e81364b8e4db3af2bfafcf8e47a
etarasova88 <eatarasova88@gmail.com>
2019-05-26 23:48:06 +0300

    completed Task11

---
+++ src/Task11.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task11 {
+
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите текст: " + "\n");
+        String text = in.nextLine();
+
+        System.out.println("Вы написали: " + text);
+    }
+
+}
