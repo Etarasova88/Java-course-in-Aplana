b290227785bb84ca632f7fa7eeeeef2396e64629
etarasova88 <eatarasova88@gmail.com>
2019-05-29 23:10:41 +0300

    fixed notes

--- src/Task10.java
+++ src/Task10.java
@@ -12,7 +12,7 @@ public class Task10 {
         System.out.print("Введите количество строк: ");
         int number2 = Integer.parseInt(in.nextLine());
 
-        int multiply[][] = new int[number2][number1];
+        int [][] multiply = new int[number2][number1];
 
         System.out.println("Введите числовые значения через Enter в количестве " + number1*number2 + "шт:");
 
@@ -24,10 +24,9 @@ public class Task10 {
         }
 
         System.out.println("Элементы массива с результатом умножения на 3:");
-       // for (int i = 0; i < number2; i++) {
          for (int j = 0; j < number1; j++) {
                 System.out.print(multiply[0][j] + " * 3 = " + multiply[0][j]*3 + ", ");
-           //}
+
         }
     }
 }
--- src/Task9.java
+++ src/Task9.java
@@ -7,14 +7,14 @@ public class Task9 {
 
         Scanner in = new Scanner(System.in);
         System.out.print("Сколько значений вы хотите ввести: ");
-        //       String t1 = in.nextLine();
+
         int number = Integer.parseInt(in.nextLine());
-        int mult[] = new int[number];
+        int [] multiply = new int[number];
 
         System.out.println("Введите числа через Enter в количестве " + number + "шт: \n");
 
         for (int i = 0; i < number; i++) {
-            mult[i] = Integer.parseInt(in.nextLine());
+            multiply[i] = Integer.parseInt(in.nextLine());
 
         }
 
@@ -22,7 +22,7 @@ public class Task9 {
         System.out.println("Элементы массива с результатом умножения на 2: \n");
         for (int i = 0; i < number; i++)
         {
-           System.out.println(mult[i]+"* 2 = " + mult[i]*2 +"\n");
+           System.out.println(multiply[i]+" * 2 = " + multiply[i]*2 +"\n");
 }
     }
 }
